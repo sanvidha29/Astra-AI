15a59e1bc2e7244c26ba6633bd55f86d0f507be2
sanvidha gaikwad <sanvidhapgaikwad@gmail.com>
2026-08-11 15:01:24 +0530

    Fix Vercel deployment

--- app.py
+++ app.py
@@ -3,43 +3,36 @@ from routes.home import home_bp
 from routes.settings import settings_bp
 from routes.voice import voice_bp
 from routes.chat import chat_bp
-from ai_engine import (
-    generate_chat_reply,
-    generate_voice_reply
-)
+
 from flask import (
     Flask,
-    render_template,
-    request,
     redirect,
     url_for,
-    session,
-    flash
+    session
 )
+
 from database import (
     create_database,
-    owner_exists,
-    create_owner,
-    get_user,
-    save_chat,
-    get_chat_history
+    owner_exists
 )
-from flask import jsonify
+
 from commands.command_handler import execute_command
 
-
-
+# Create Flask App
 app = Flask(__name__)
 
+# Create database
+create_database()
+
+# Secret Key
 app.secret_key = "astra_ai_secret_key"
+
+# Register Blueprints
 app.register_blueprint(home_bp)
 app.register_blueprint(auth_bp)
 app.register_blueprint(settings_bp)
 app.register_blueprint(voice_bp)
 app.register_blueprint(chat_bp)
-
-
-
 
 
 @app.route("/start")
@@ -48,7 +41,6 @@ def start():
     if not owner_exists():
         return redirect(url_for("auth.setup"))
 
-
     if "username" not in session:
         return redirect(url_for("auth.login"))
 
@@ -56,9 +48,4 @@ def start():
 
 
 if __name__ == "__main__":
-    create_database()
-    # from ai_engine import show_models
-    # show_models()
     app.run(debug=True)
-
-
